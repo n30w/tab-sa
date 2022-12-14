f29716cc85911af1859f76c235cab341991c24e3
n30w <rocketneo@gmail.com>
2022-12-14 03:56:10 -1000

    Added test to check if Givebox site still exists

--- tests/paths.spec.ts
+++ tests/paths.spec.ts
@@ -23,3 +23,11 @@ test.describe('Navbar', () => {
     })
 
 });
+
+test('check if Givebox site still exists', async ({ page }) => {
+
+  await page.goto('https://givebox.com/517849');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('SOUTH AFRICA PROGRAM | Teachers Across Borders');
+});
