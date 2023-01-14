0bc4bb891a94a0b2e9ac0380512b74ac3ee4bfa5
n30w <rocketneo@gmail.com>
2023-01-14 00:49:42 -1000

    changed project page header

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,7 +4,10 @@ export default function Projects() {
       <div className="page">
         <div className="content">
           <div className="sectionHeader">
-            <h1>Our current projects</h1>
+            <h1>
+              <span className="text-tabTheme-200">Where We Are,</span>{" "}
+              <span className="text-tabTheme-100">Where We&apos;re Going.</span>
+            </h1>
           </div>
         </div>
       </div>
