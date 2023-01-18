f36fdd4e5f9c0a4b42d8d27e7cdf3ee483afc189
n30w <rocketneo@gmail.com>
2023-01-17 18:23:37 -1000

    homepage no longer renders footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,40 +1,46 @@
 import Link from "next/link";
 import Image from "next/image";
 import { navigation } from "./navbar";
+import { useRouter } from "next/router";
 
 export default function Footer() {
+  const router = useRouter();
+
   return (
     <>
-      <div className="footer p-14 pb-10 hidden md:pb-10 bg-base-200 text-base-content font-atkinson md:grid grid-cols-1 md:grid-cols-5 justify-items-bottom">
-        <div className="hidden md:flex items-center justify-center">
-          <Image
-            alt="Teachers Across Borders Logo"
-            src="/Logo.svg"
-            className="scale-50"
-            width={312}
-            height={335}
-          />
-        </div>
-        <div className=""></div>
+      {/* Do not render Footer on homepage */}
+      {router.pathname !== "/" && (
+        <div className="footer p-14 pb-10 hidden md:pb-10 bg-base-200 text-base-content font-atkinson md:grid grid-cols-1 md:grid-cols-5 justify-items-bottom">
+          <div className="hidden md:flex items-center justify-center">
+            <Image
+              alt="Teachers Across Borders Logo"
+              src="/Logo.svg"
+              className="scale-50"
+              width={312}
+              height={335}
+            />
+          </div>
+          <div className=""></div>
 
-        <div className="flex flex-col pt-8">
-          <span className="footer-title">
-            <span style={{ fontWeight: 700, textTransform: "none" }}>
-              Teachers Across Borders
-            </span>{" "}
-            <span style={{ fontWeight: 400, textTransform: "none" }}>
-              Southern Africa
+          <div className="flex flex-col pt-8">
+            <span className="footer-title">
+              <span style={{ fontWeight: 700, textTransform: "none" }}>
+                Teachers Across Borders
+              </span>{" "}
+              <span style={{ fontWeight: 400, textTransform: "none" }}>
+                Southern Africa
+              </span>
             </span>
-          </span>
-          <div className="flex flex-flow md:flex-col">
-            {navigation.map((item) => (
-              <Link key={item.name} href={item.href} className="">
-                <div className="link link-hover">{item.name}</div>
-              </Link>
-            ))}
+            <div className="flex flex-flow md:flex-col">
+              {navigation.map((item) => (
+                <Link key={item.name} href={item.href} className="">
+                  <div className="link link-hover">{item.name}</div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="">
+        <div className="block">
           <Image
             src={bg}
             style={{
@@ -22,63 +22,8 @@ export default function Home() {
             fill={true}
             className="-z-10"
           />
-          <div className="mx-auto 2xl:h-screen pt-12">
-            <div>
-              <div className="flex flex-col justify-start space-y-2 pt-7 text-white px-10">
-                <div className="h-7 2xl:h-28"></div>
-                <h1 className="text-2xl tracking-normal text-left sm:text-3xl 2xl:text-5xl font-atkinson font-bold">
-                  We change communities through education
-                </h1>
-              </div>
-            </div>
-          </div>
         </div>
       </main>
-      <div className="hero min-h-screen content-around">
-        <div className="hero-content flex flex-col">
-          <div className="flex justify-center">
-            <Image
-              src="/Logo.svg"
-              className=""
-              width={312}
-              height={335}
-              alt={"Teachers Without Borders Logo"}
-            />
-          </div>
-          <p className="text-2xl sm:text-3xl lg:text-4xl px-2">
-            We are a group of educators born from the legacy of Apartheid. Every
-            US summer, a group of math and science educators from Hawaii and
-            around the US travel to Southern Africa to conduct workshops with
-            our Southern African colleagues in rural schools.
-          </p>
-          <div className="max-w-auto"></div>
-        </div>
-      </div>
-      <div className="hero min-h-screen content-around">
-        <div className="hero-content flex flex-col">
-          <p className="text-2xl sm:text-3xl lg:text-3xl px-2">
-            The Republic of South Africa is at a critical juncture in its
-            history. The national government has identified the teaching and
-            learning of math and science as priorities in the educational needs
-            of the country. While millions are no longer legally marginalized by
-            the educational system, a massive infusion of material support and
-            professional training of teachers and administrators is required,
-            quickly, efficiently, and effectively to appropriately serve the
-            long-neglected majority population of school children.
-          </p>
-          <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-xl leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            <span className="text-gray-600">
-              Learn more of our story.{" "}
-              <Link href="/about" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                About us <span aria-hidden="true">&rarr;</span>
-              </Link>
-            </span>
-          </div>
-
-          <div className="max-w-auto"></div>
-        </div>
-      </div>
     </>
   );
 }
