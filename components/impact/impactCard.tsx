0d1aadd10ed453a89950bb495465286a71c779a7
n30w <rocketneo@gmail.com>
2023-01-18 16:48:52 -1000

    modified impact grid

--- components/impact/impactCard.tsx
+++ components/impact/impactCard.tsx
@@ -3,7 +3,7 @@ import serialize from "../../lib/serialize";
 export default function impactCard({ post }) {
   return (
     <>
-      <div className="w-screen h-60 bg-base-100 border-2 border-black hover:bg-[#ffeaba] rounded-md">
+      <div className="w-full md:w-screen h-32 bg-base-100 border-2 border-black hover:bg-[#ffeaba] rounded-md">
         <div className="card-body items-center">
           <h2 className="card-title font-normal text-5xl">{post.author}</h2>
           {/* <p>{serialize(post.body, "impact")}</p> */}
